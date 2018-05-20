7befef82fcf07e4d545452fcbc322f2d645f4cad
Eric Chang <efrochang@gmail.com>
2018-05-20 14:22:14 -0500

    Login Logout buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,8 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor( private router: Router ) { }   // [KEY]: Inject Router
+  constructor( private router: Router,
+               private authService: AuthService ) { }   // [KEY]: Inject Router
 
   ngOnInit() {
   }
@@ -22,5 +24,15 @@ export class HomeComponent implements OnInit {
         fragment: 'loading'
       }
      );     
+  }
+
+  onLogin()
+  {
+    this.authService.login();
+  }
+
+  onLogout()
+  {
+    this.authService.logout();
   }
 }
